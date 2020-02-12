2013164d9f294e90bd2224a1b80e36b0b6b57ab7
DreamsInCS <yoannierhermida@knights.ucf.edu>
2020-02-12 13:52:03 -0500

    some API call processing completed

---
+++ lib/src/app.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class App extends StatelessWidget {
+  Widget build (context) {
+    return MaterialApp(
+      home: Text('\n\n\n\n\nNick is dumb.')
+    );
+  }
+}
---
+++ test/src/resources/news_api_provider_test.dart
@@ -0,0 +1,6 @@
+// Importing from outside of lib? Use package: !
+import 'package:news/src/resources/news_api_provider.dart';
+import 'dart:convert';
+import 'package:test/test.dart';
+import 'package:http/http.dart';
+import 'package:http/testing.dart';
